0eb0643cc8bfab1dd889fd1d668af0ca63e5b1bf
Jarek Torres <90516894+AestheticLUV@users.noreply.github.com>
2022-01-24 12:49:16 -0500

    myInfo version 3

--- myInfo.py
+++ myInfo.py
@@ -2,4 +2,6 @@
 #Jarek Torres
 #myInfo
 print("Hello my name is Jarek Torres")
-print("I am a student")
+print("My address is 39 Morris St , Paterson, New Jersey 07501")
+print("My telephone is 862-291-5145")
+print("My College Major is Software Engineering")

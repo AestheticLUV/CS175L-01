c00c21e448ae5e05e0e9a9bdd487b44433bc18a8
Jarek Torres <90516894+AestheticLUV@users.noreply.github.com>
2022-01-27 22:56:22 -0500

    Stocks.py

---
+++ Stocks.py
@@ -0,0 +1,33 @@
+#CS-175L-01
+#Jarek Torres
+#Stocks
+
+COMMISSION_RATE = 0.03 #the commission rate 
+NUM_SHARES = 2000  #number of shares bought 
+PURCHASE_PRICE = 40.0  #how much each share cost
+SELLING_PRICE = 42.75  #how much each share were sold for 
+
+
+amountPaidForStock = 80000.0  #amount paid for stock
+purchaseCommission = 2400.0   #the commission paid for buying shares
+amountStockSoldFor = 85500.0 # amount the stocks are sold for
+saleCommission = 2565.0 #the commision paid for sale
+TotalCommission = 4965.0  #total cost of comission paid / purchase + sale 
+Profit = 535.0
+
+print("What was the commission rate?:" + " " + str(COMMISSION_RATE))
+print('''How many shares did you purchase?:''' + " " + str(NUM_SHARES))
+print("What was the purchase price?:" + " " + str(PURCHASE_PRICE))
+print("What was the selling price?:" + " " + str(SELLING_PRICE))
+print("Amount Paid for Stock:" + " $ " + str(amountPaidForStock))
+print("Commision paid on the purchase:" + " $ " + str(purchaseCommission))
+print("Amount the stock sold for:" + " $ " + str(amountStockSoldFor))
+print("Commission paid on the sale:" + " $ " + str(saleCommission))
+print("Total commission paid:" + " $ " + str(TotalCommission))
+print("Profit (or loss if negative):" + " $ " + str(Profit))
+
+
+
+
+
+

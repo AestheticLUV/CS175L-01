23e80d9812e47dce2ff43c7e7af5d3f4e5124616
Jarek Torres <90516894+AestheticLUV@users.noreply.github.com>
2022-03-28 01:21:04 -0400

    Average From Input File Part 2
    This program handle errors

--- Average From Input File**.py
+++ Average From Input File**.py
@@ -1,33 +1,44 @@
 #CS-175LAB
 #Jarek Torres
-#Average From Input File
-
-#NOTES
-# The num3 would not print out the number thats why i put the -99 instead of float(file.readline()) #LINE 17
-#The other numbers such as num1 and num2 print out perfect in the for loop
+#Average From Input File with Exception Handling
 
 
 
+#It should handle any IOError exception that are rasied when the file is opened and data is read from it
+#It should handle any ValueError exceptions that are raised when the items that are read from the file are converted to a number
 def main():
+    count = 0
+    #total = 0 
+    average = 0
+    try:
+        file = open("numbers.txt", 'r')
+        num1 = float(file.readline(4))
+        num2 = float(file.readline(8))
+        num3 = float(-99)
+        
+            
+        
+        for files in file:
+            #total = num1 + num2 + num3
+            a = num1+num2
+            b = a+num3
+            count = num1 + num2 + num3
+            #count= +1 
+            average = count/3
+            print('\n') #used for spacing 
+            print(f'I read in 1 number(s) Current number is: {num1:.2f} Total is: {num1:.2f}')
+            print(f'I read in 2 number(s) Current number is: {num2:.2f} Total is: {a:.2f}')
+            print(f'I read in 3 number(s) Current number is: {num3:.2f} Total is: {b:.2f}')
+            print(f'Average: {average}')
+            
+       
+    except IOError:
+        print("File could not open try again!") #prints this when it has problems open the txt.file
 
-    file = open('numbers.txt', 'r')
-
-    num1 = float(file.readline(4))
-    num2 = float(file.readline(8))
-    num3 = -99          #float(file.readline(13))  #I tried to print the line but it didnt print for me
-
-
-    for files in file:
-    #print(files)    
-        total = num1 + num2 + num3
-        a = num1+num2
-        b = a+num3
-        count = num1 + num2 + num3 
-        average = count/3
-        print(f'I read in 1 number(s) Current number is: {num1:.2f} Total is: {num1:.2f}')
-        print(f'I read in 2 number(s) Current number is: {num2:.2f} Total is: {a:.2f}' )
-        print(f'I read in 3 number(s) Current number is: {num3:.2f} Total is: {b:.2f}')
-        print(f'Average: {average}')
-        break
+    except ValueError:
+        print("No number found in file try again!") #prints this when the txt has a non-number in it
+    
+    file.close()
+        
 if __name__ == '__main__':
     main()
